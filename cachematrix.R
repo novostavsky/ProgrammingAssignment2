5d5713b931fd14321d355fb523961c9b5aa0bf0b
novostavsky <novostavsky@gmail.com>
2015-01-25 19:53:39 +0200

    Update asmnt #2
     makeCacheMatrix and cacheSolve functions, add human comments, remove "ToDo" comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## coursera.org - rprog-010 - asmnt2
 
-## Write a short comment describing this function
+## These funcations are written to speed up calculations od
+## inverted matrices. The first one is a special function
+## containing spec "matrices", and the second one calculates
+## inversions for these matrices. See details below.
 
+
+##The function makeCacheMatrix contains a number of internal
+##functions "getters" and "setters" for matrix itself
+##and cached value of inverted one (if was calculated before) 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    ##setter for matrix
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    ##getter for matrix
+    get <- function() x
+    ##setter for inverted matrix
+    setinverse <- function(inverse) inv <<- inverse
+    ##getter for inverted matrix
+    getinverse <- function() inv
+    ##the resulting list
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+##The cacheSolve funcation gets a matrix and 
+##returns inverted one. If the inverted was
+##calculated before, the function gets cached version
+##to improve speed/performance.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ##try to get inverted matrix from cache
+    inv <- x$getinverse()
+    ##check if it's null (wasn't calculated)
+    ##if sucess -> print the mesage and return cache
+    if(!is.null(inv)){
+        message("get chached inverse")
+        return(inv)
+    }
+    ##if fail -> we need to calculate it 
+    data <- x$get()
+    inv <- solve(data)
+    ##and update cache with calculated value
+    x$setinverse(inv)
+    ##return a matrix that is the inverse of 'x'
+    inv
 }
